ba4ac2620d92dd18aedaab2a39784232b4230087
anzonghui123 <15138071321@163.com>
2017-09-13 08:49:49 +0800

    gai le dian

--- aaa.py
+++ aaa.py
@@ -1,7 +1,7 @@
 # -*- coding:UTF-8 -*-
 for i in range(1,10):
     for j in range(1,i+1):
-        result = i * j
-        print j, 'x', i, '=', j*i ,'\t',
+        result = j * i
+        print j,'*',i,'=',j*i,'\t',
     print ''
 
